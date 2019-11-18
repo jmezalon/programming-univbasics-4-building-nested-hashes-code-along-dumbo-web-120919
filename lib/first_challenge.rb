58d12ab2a36e1b0d3422e5b7a2e2ac6be6dfd3e1
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-18 13:39:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,3 +5,22 @@ def first_challenge
     :capulet => {}
   }
 end
+
+
+def second_challenge
+   epic_tragedy = {
+     
+    :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+    },
+    :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+    }
+  }
+end
